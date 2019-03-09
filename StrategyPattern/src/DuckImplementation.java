cdfcbee4ca6a696ad0b50b77bbc0a5c7ffef48c6
Vignesh <a.vignesh@fundsindia.com>
2019-03-09 23:15:52 +0530

    Comments

--- StrategyPattern/src/DuckImplementation.java
+++ StrategyPattern/src/DuckImplementation.java
@@ -2,9 +2,18 @@ public class DuckImplementation {
 
 
     public static void main(String[] args){
-        IFlyBehaviour flyBehaviour = new SimpleFlyBehaviour();
-        IQuackBehaviour quackBehaviour = new ComplexQuackBehaviour();
-        DuckBehaviourModule duckBehaviourModule = new DuckBehaviourModule(flyBehaviour, quackBehaviour);
-        System.out.println(duckBehaviourModule.toString());
+
+        // Create instances for constructor injection
+        IFlyBehaviour simpleFlyBehaviour = new SimpleFlyBehaviour(); // Horizontal sharing of code which is not possible with inheritance
+        IQuackBehaviour simpleQuackBehaviour = new SimpleQuackBehaviour();
+        IQuackBehaviour complexQuackBehaviour = new ComplexQuackBehaviour();
+
+        // Create instances of the classes with different behaviours
+        DuckBehaviourModule simpleDuckBehaviour = new DuckBehaviourModule(simpleFlyBehaviour, simpleQuackBehaviour);
+        DuckBehaviourModule complexDuckBehaviour = new DuckBehaviourModule(simpleFlyBehaviour, complexQuackBehaviour);
+
+        // Print them to check if they are working fine
+        System.out.println(simpleDuckBehaviour.toString());
+        System.out.println(complexDuckBehaviour.toString());
     }
 }
